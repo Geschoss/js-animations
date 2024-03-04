65f3f26c65e97712d57d2a0280eba39d76232efa
Pavel Kolomnikov <q11254@gmail.com>
2024-03-04 12:06:43 +0700

    frictions

--- src/pages/simple_examples/examples/chapter_6/boundaties.ts
+++ src/pages/simple_examples/examples/chapter_6/boundaties.ts
@@ -1,6 +1,7 @@
 import { LinearMove } from 'src/entities/engine/2d/behaviors';
 import { Engine2D, Env } from 'src/entities/engine/2d/engine';
 import { Ball, Rect } from 'src/entities/engine/2d/entities';
+import { View } from 'src/entities/engine/2d/view';
 import { initByCb, r_range } from 'src/shared/lib';
 
 export class Bundaries {
@@ -17,8 +18,9 @@ export class Bundaries {
     this.screen = createScreen(this.game2D.env);
 
     const BALL_RADIUS = 10;
-    const BALL_COUNT = 100;
-    const BALL_MAX_SPEED = 10;
+    const BALL_COUNT = 30;
+    const BALL_MAX_SPEED = 3;
+    const BALL_FRICTION = 0.9;
 
     this.balls = initByCb(
       BALL_COUNT,
@@ -34,7 +36,8 @@ export class Bundaries {
           new LinearMove<Ball>(
             r_range(-BALL_MAX_SPEED, BALL_MAX_SPEED),
             r_range(-BALL_MAX_SPEED, BALL_MAX_SPEED)
-          )
+          ),
+          new BallViewWithLine(),
         )
     );
 
@@ -42,10 +45,12 @@ export class Bundaries {
       this.screen?.render(context.ctx);
       this.balls.forEach((ball) => {
         if (is_leaf_x(ball, this.screen)) {
-          ball.behavior.dx = -ball.behavior.dx;
+          ball.behavior.dx = -BALL_FRICTION * ball.behavior.dx;
+          ball.behavior.dy = BALL_FRICTION * ball.behavior.dy;
         }
         if (is_leaf_y(ball, this.screen)) {
-          ball.behavior.dy = -ball.behavior.dy;
+          ball.behavior.dx = BALL_FRICTION * ball.behavior.dx;
+          ball.behavior.dy = -BALL_FRICTION * ball.behavior.dy;
         }
         ball.think();
         ball.render(context.ctx);
@@ -81,20 +86,53 @@ function createScreen(env: Env) {
 }
 
 function is_leaf_x(ball: Ball, screen: Rect) {
-  if (screen.x > ball.x - ball.radius) {
+  if (screen.x > ball.x - ball.radius && ball.behavior.dx < 0) {
     return true;
   }
-  if (screen.right < ball.x + ball.radius) {
+  if (screen.right < ball.x + ball.radius && ball.behavior.dx > 0) {
     return true;
   }
   return false;
 }
 function is_leaf_y(ball: Ball, screen: Rect) {
-  if (screen.y > ball.y - ball.radius) {
+  if (screen.y > ball.y - ball.radius && ball.behavior.dy < 0) {
     return true;
   }
-  if (screen.bottom < ball.y + ball.radius) {
+  if (screen.bottom < ball.y + ball.radius && ball.behavior.dy > 0) {
     return true;
   }
   return false;
+}
+
+export class BallViewWithLine implements View<Ball> {
+  render(ctx: CanvasRenderingContext2D, { x, y, radius, color, behavior }: Ball) {
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.closePath();
+    ctx.restore();
+
+    ctx.beginPath();
+    ctx.strokeStyle = '#0000ff';
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + behavior.dx * 100, y);
+    ctx.closePath();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x, y + behavior.dy * 100);
+    ctx.strokeStyle = '#f00fff';
+    ctx.closePath();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + behavior.dx * 100, y + behavior.dy * 100);
+    ctx.strokeStyle = '#ff0000';
+    ctx.closePath();
+    ctx.stroke();
+  }
 }
